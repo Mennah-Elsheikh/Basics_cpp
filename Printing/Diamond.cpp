f774b7adefc612ea898e4b0d3cf93f673da8b44b
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2024-02-13 16:26:11 +0200

    Add files via upload

---
+++ Printing/Diamond.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+#include <iostream>
+int main()
+{
+    cout<<"        *        "<<endl;
+	cout<<"      * * *      "<<endl;
+	cout<<"    * * * * *    "<<endl;
+	cout<<"  * * * * * * *  "<<endl;
+	cout<<"* * * * * * * * *"<<endl;
+	cout<<"  * * * * * * *  "<<endl;
+	cout<<"    * * * * *    "<<endl;
+	cout<<"      * * *      "<<endl;
+	cout<<"        *        "<<endl;
+
+}
---
+++ Printing/Traingle.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+int main()
+{
+    cout << "*" << endl;
+    cout << "* *" << endl;
+    cout << "* * *" << endl;
+    cout << "* * * *" << endl;
+    cout << "* * * * *" << endl;
+}
